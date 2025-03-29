a412187559592e3eaae85e4e1d070e7e0adecbf7
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2025-03-29 08:29:16 +0000

    Hidden extra pages which aren't in use yet

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pubsource=https://github.com/HL7/fhir-ig-publisher/releases/latest/download/
+pubsource=https://github.com/HL7/fhir-ig-publisher/releases/download/1.8.20/
 publisher_jar=publisher.jar
 dlurl=$pubsource$publisher_jar
 
