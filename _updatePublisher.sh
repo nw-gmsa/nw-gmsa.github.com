61fd6c83ca780bfc1438f00c492f1e0d550d41dd
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2026-06-15 08:01:43 +0100

    Removed non published plantuml and markdown

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pubsource=https://github.com/HL7/fhir-ig-publisher/releases/latest/download/
+pubsource=https://github.com/HL7/fhir-ig-publisher/releases/download/2.2.8/
 publisher_jar=publisher.jar
 dlurl=$pubsource$publisher_jar
 
